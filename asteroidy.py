ae1aa36c8becbe799f62d14342e867b79b49b857
Marrek Nožka <nozka@spseol.cz>
2018-04-26 14:07:18 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Soubor:  asteroidy.py
+# Datum:   26.04.2018 13:26
+# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
+# Licence: GNU/GPL
+############################################################################
+
+import pyglet
+
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
